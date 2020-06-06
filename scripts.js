859d30d2573ad5392a2c30fc88288b14f5880566
aaron.harpt <aaron.harpt@gmail.com>
2020-06-06 10:57:55 -0500

    Worked on html, css, and jquery

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$("#submit button").click(function(event) {
+  event.preventDefault();
+
+  if ($("#form-name-block input").val() === "") {
+    console.log("Name is empty");
+
+    $("#form-name-block input").addClass("error");
+  }
+
+  console.log($("#form-name-block input").val());
+});
