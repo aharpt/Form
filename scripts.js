8799878878187a333c8556091d5c16e2431fa456
aaron.harpt <aaron.harpt@gmail.com>
2020-06-06 18:56:21 -0500

    Changed comment

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ if ($("#form-name-block input").val() !== "") {
 
   console.log($("#form-name-block input").val());
 
-  // Got regular expression from Stack Overflow post https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+  // Got regular expression from Stack Overflow https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const email = $("#form-email-block input").val();
 
